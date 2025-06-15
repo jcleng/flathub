cc49e6e56b2853dba8d2e2b23eda20c2be8dda43
jcleng <j.c.leng@foxmail.com>
2025-06-16 00:08:56 +0800

    Add: com.todesk.todesk
    Signed-off-by: jcleng <j.c.leng@foxmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env sh
-export APPDIR="/app/dir"
-exec /app/dir/AppRun --no-sandbox
+
+export TODESK_PACK_NAME=todesk
+TODESK_PATH=/app/$TODESK_PACK_NAME
+TODESK_BIN_PATH=$TODESK_PATH/bin
+TODESK_CONF_PATH=$XDG_CONFIG_HOME
+
+
+# 后台
+$TODESK_BIN_PATH/todeskc $TODESK_BIN_PATH/todeskd $TODESK_CONF_PATH/todeskd.conf
+/app/todesk/bin/todeskd -f $XDG_CONFIG_HOME/todeskd.conf &
+
+# 前台
+sleep 4
+$TODESK_BIN_PATH/todesk
+pkill todeskd
+exit
